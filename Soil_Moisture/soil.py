37740e51679cb3757d3ec1328944cb3894ea8718
DESKTOP-U801D1U\nico_ <nico_ntrax@hotmail.com>
2021-01-02 11:51:59 +0100

    project for soil moisture

--- Soil_Moisture/soil.py
+++ Soil_Moisture/soil.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
+import datetime
  
 #GPIO SETUP
 channel = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
  
-def callback(channel):
-        if GPIO.input(channel):
-                print "Water Detected!"
-        else:
-                print "Water Detected!"
- 
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
- 
+
 # infinite loop
 while True:
         time.sleep(1)
+
+        msg = ""
+
+        if GPIO.input(channel):
+                # Input is high
+                msg = str(datetime.datetime.now()) + " - No Water Detected!"
+                print msg
+        else:
+                # Input is low
+                msg = str(datetime.datetime.now()) + " - Water Detected!"
+                print msg
+
+        f = open("soil_moisture.log", "a")
+        f.write(msg)
+        f.close()
---
+++ Soil_Moisture/soil_v2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+import datetime
+ 
+#GPIO SETUP
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+ 
+def callback(channel):
+        if GPIO.input(channel):
+                # Input is high
+                print str(datetime.datetime.now()) + " - No Water Detected!"
+        else:
+                # Input is low
+                print str(datetime.datetime.now()) + " - Water Detected!"
+ 
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+ 
+# infinite loop
+while True:
+        time.sleep(1)
+        if GPIO.input(channel):
+                # Input is high
+                print str(datetime.datetime.now()) + " - No Water Detected!"
+        else:
+                # Input is low
+                print str(datetime.datetime.now()) + " - Water Detected!"
